b1d3ba4a4373007acc10edb7e9353fd0d98a3cec
Jacob Capps <99674188+jcbcapps@users.noreply.github.com>
2022-08-23 14:51:27 -0500

    Update test for sc-771/display-latest-articles (#142)
    * Update test
    
    * Experimenting
    
    * Fix error
    
    * More changes
    
    * More stuff
    
    * More stuff
    
    * Remove echo
    
    * More stuff
    
    * More stuff
    
    * ci: update the ci e2e workflow to support a branch accross repos or main
    
    * Update getNewsRSS
    
    Co-authored-by: John Gedeon <john@truss.works>

--- e2e/cypress/integration/news.spec.js
+++ e2e/cypress/integration/news.spec.js
@@ -1,90 +1,90 @@
-import logging from '../plugins/logging'
+import logging from "../plugins/logging";
 
-describe('News and Announcements', () => {
+describe("News and Announcements", () => {
   before(() => {
-    cy.loginTestIDP()
-  })
+    cy.loginTestIDP();
+  });
 
   beforeEach(() => {
-    cy.preserveLoginCookies()
+    cy.preserveLoginCookies();
 
     cy.intercept(
-      'https://www.spaceforce.mil/DesktopModules/ArticleCS/RSS.ashx?ContentType=1&Site=1060&max=12'
-    ).as('getNewsRSS')
+      "https://www.spaceforce.mil/DesktopModules/ArticleCS/RSS.ashx?ContentType=1&Site=1060&max=4"
+    ).as("getNewsRSS");
 
     cy.intercept(
-      'https://www.spaceforce.mil/DesktopModules/ArticleCS/RSS.ashx?ContentType=1&Site=1060&max=2'
-    ).as('getNewsWidgetRSS')
+      "https://www.spaceforce.mil/DesktopModules/ArticleCS/RSS.ashx?ContentType=1&Site=1060&max=2"
+    ).as("getNewsWidgetRSS");
 
-    cy.visit('/')
-    cy.injectAxe()
-  })
+    cy.visit("/");
+    cy.injectAxe();
+  });
 
-  it('can visit the News page', () => {
-    cy.findByRole('link', { name: 'News' }).click()
-    cy.url().should('eq', Cypress.config().baseUrl + '/news-announcements')
-    cy.injectAxe()
-    cy.findByRole('heading', { level: 1 }).contains('News')
-    cy.wait('@getNewsRSS')
-    cy.findAllByRole('article').should('exist')
-    cy.checkA11y(null, null, logging, { skipFailures: true })
-  })
+  it("can visit the News page", () => {
+    cy.findByRole("link", { name: "News" }).click();
+    cy.url().should("eq", Cypress.config().baseUrl + "/news-announcements");
+    cy.injectAxe();
+    cy.findByRole("heading", { level: 1 }).contains("News");
+    cy.wait("@getNewsRSS");
+    cy.findAllByRole("article").should("exist");
+    cy.checkA11y(null, null, logging, { skipFailures: true });
+  });
 
-  it('can add the News Section to My Space', () => {
-    cy.contains('My Space')
-    cy.findByRole('button', { name: 'Add section' }).click()
-    cy.findByRole('button', { name: 'Add news section' }).click()
+  it("can add the News Section to My Space", () => {
+    cy.contains("My Space");
+    cy.findByRole("button", { name: "Add section" }).click();
+    cy.findByRole("button", { name: "Add news section" }).click();
 
-    cy.wait('@getNewsWidgetRSS')
+    cy.wait("@getNewsWidgetRSS");
 
-    cy.findByRole('heading', { level: 3, name: 'Recent News' }).should('exist')
+    cy.findByRole("heading", { level: 3, name: "Recent News" }).should("exist");
 
-    cy.contains('Recent News')
+    cy.contains("Recent News")
       .parent()
       .parent()
       .within(() => {
-        cy.findAllByRole('article').should('have.length', 2)
-      })
+        cy.findAllByRole("article").should("have.length", 2);
+      });
 
-    cy.findByRole('button', { name: 'Add section' }).click()
-    cy.findByRole('button', { name: 'Add news section' }).should('be.disabled')
-  })
+    cy.findByRole("button", { name: "Add section" }).click();
+    cy.findByRole("button", { name: "Add news section" }).should("be.disabled");
+  });
 
-  it('can remove the News Section from My Space', () => {
-    cy.contains('My Space')
+  it("can remove the News Section from My Space", () => {
+    cy.contains("My Space");
 
-    cy.contains('Recent News')
+    cy.contains("Recent News")
       .parent()
       .parent()
       .within(() => {
-        cy.findByRole('button', { name: 'Section Settings' }).click()
-        cy.findByRole('button', { name: 'Remove this section' }).click()
-      })
+        cy.findByRole("button", { name: "Section Settings" }).click();
+        cy.findByRole("button", { name: "Remove this section" }).click();
+      });
 
     // Cancel first to make sure it's possible
-    cy.findByRole('dialog', {
-      name: 'Are you sure you’d like to delete this section?',
+    cy.findByRole("dialog", {
+      name: "Are you sure you’d like to delete this section?",
     }).within(() => {
-      cy.findByRole('button', { name: 'Cancel' }).click()
-    })
+      cy.findByRole("button", { name: "Cancel" }).click();
+    });
 
     // Reopen modal
-    cy.contains('Recent News')
+    cy.contains("Recent News")
       .parent()
       .parent()
       .within(() => {
-        cy.findByRole('button', { name: 'Section Settings' }).click()
-        cy.findByRole('button', { name: 'Remove this section' }).click()
-      })
+        cy.findByRole("button", { name: "Section Settings" }).click();
+        cy.findByRole("button", { name: "Remove this section" }).click();
+      });
 
     // Delete
-    cy.findByRole('dialog', {
-      name: 'Are you sure you’d like to delete this section?',
+    cy.findByRole("dialog", {
+      name: "Are you sure you’d like to delete this section?",
     }).within(() => {
-      cy.findByRole('button', { name: 'Delete' }).click()
-    })
+      cy.findByRole("button", { name: "Delete" }).click();
+    });
 
     // Make sure it has been deleted
-    cy.contains('Recent News').should('not.exist')
-  })
-})
+    cy.contains("Recent News").should("not.exist");
+  });
+});
