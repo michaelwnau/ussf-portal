46d3530f75b1528a54e606f6b0dba2270594cbd6
Jacob Capps <99674188+jcbcapps@users.noreply.github.com>
2022-12-02 16:02:41 -0600

    test: Convert sites-and-appilcations Cypress test over to Playwright (#168)
    * Add new folder for portal tests
    
    * Move cms e2e tests and update playwright config
    
    * Update config and add initial test
    
    * Update db name
    
    * Change name back
    
    * Update db name
    
    * Remove one of the Login.ts files and update import
    
    * Add test for toggling theme
    
    * Update assertion
    
    * Convert test to playwright
    
    * Convert test to playwright
    
    * Convert test to playwright
    
    * Convert test to playwright
    
    * Convert test
    
    * Convert test
    
    * Convert test
    
    * Convert test
    
    * Remove unnecessary test
    
    * Combine edit custom link test w/ add custom link test
    
    * Combine delete custom links test w/ add/edit custom links test
    
    * Convert test to playwright
    
    * Convert test to playwright
    
    * Convert test to playwright
    
    * Add check for undo remove

--- e2e/playwright/portal-client/tests/myspace.spec.ts
+++ e2e/playwright/portal-client/tests/myspace.spec.ts
@@ -21,18 +21,16 @@ const test = base.extend<TestingLibraryFixtures & CustomFixtures>({
 
 const { describe, expect } = test
 
+const waitFor = (delay: number) => {
+  return new Promise((resolve) => setTimeout(resolve, delay))
+}
+
 test.beforeAll(async () => {
   await resetDb()
   await seedDB()
 })
 
 describe('MySpace', () => {
-  test('can login and see MySpace', async ({ page, loginPage }) => {
-    await loginPage.login('user1', 'user1pass')
-    await expect(page.locator('text=WELCOME, BERNIE')).toBeVisible()
-    await expect(page.locator('text=Example Collection')).toBeVisible()
-  })
-
   test('can toggle light/dark mode', async ({ page, loginPage }) => {
     await loginPage.login('user1', 'user1pass')
     await expect(page.locator('text=WELCOME, BERNIE')).toBeVisible()
@@ -40,5 +38,199 @@ describe('MySpace', () => {
 
     await page.locator('[data-testid="theme-toggle"]').click()
     await expect(page.locator('text=LIGHT MODE')).toBeVisible()
+  })
+
+  test('can add a new custom collection to My Space', async ({
+    page,
+    loginPage,
+  }) => {
+    // Login
+    await loginPage.login('user1', 'user1pass')
+    await expect(page.locator('text=WELCOME, BERNIE')).toBeVisible()
+
+    // Create a custom collection
+    await page.locator('text=Add section').click()
+    await page.locator('text=Create new collection').click()
+    await page
+      .locator('[placeholder="Name this collection"]')
+      .fill('Playwright Test Collection')
+    await page.locator('text=Save name').click()
+    await expect(page.locator('text=Playwright Test Collection')).toBeVisible()
+  })
+
+  test('can hide links from an existing collection', async ({
+    page,
+    loginPage,
+  }) => {
+    // Login and check that user is in My Space
+    await loginPage.login('user1', 'user1pass')
+    await expect(page.locator('text=WELCOME, BERNIE')).toBeVisible()
+    await expect(page.locator('text=Example Collection')).toBeVisible()
+
+    // Remove Webmail and undo
+    await expect(
+      page.locator('text=Webmail(opens in a new window)')
+    ).toBeVisible()
+    await page.locator('[aria-label="Remove this link"]').first().click()
+    await expect(
+      page.locator('text=Webmail(opens in a new window)')
+    ).toBeHidden()
+    await page.locator('text=Undo remove').click()
+    await expect(
+      page.locator('text=Webmail(opens in a new window)')
+    ).toBeVisible()
+  })
+
+  test('can add existing links to an existing collection', async ({
+    page,
+    loginPage,
+  }) => {
+    // Login and check that user is in My Space
+    await loginPage.login('user1', 'user1pass')
+    await expect(page.locator('text=WELCOME, BERNIE')).toBeVisible()
+    await expect(page.locator('text=Example Collection')).toBeVisible()
+
+    // Open dropdown and select an existing bookmark and click it
+    await page.locator('text=+ Add link').first().click()
+    await page.locator('[data-testid="combo-box-toggle"]').click()
+    await expect(page.locator('li:has-text("ACPINS")')).toBeVisible()
+    await page.locator('li:has-text("ACPINS")').click()
+
+    // Check that dropdown is hidden and there is a new bookmark
+    await expect(page.locator('[data-testid="combo-box-toggle"]')).toBeHidden()
+    await expect(
+      page.locator('text=ACPINS(opens in a new window)')
+    ).toBeVisible()
+  })
+
+  test('can add/edit/delete custom links in an existing collection', async ({
+    page,
+    loginPage,
+  }) => {
+    // Login and check that user is in My Space
+    await loginPage.login('user1', 'user1pass')
+    await expect(page.locator('text=WELCOME, BERNIE')).toBeVisible()
+    await expect(page.locator('text=Example Collection')).toBeVisible()
+
+    // Open dropdown and select the 'Add custom link' option and click it
+    await page.locator('text=+ Add link').first().click()
+    await page.locator('[data-testid="combo-box-toggle"]').click()
+    await page.locator('[data-testid="combo-box-option-custom"]').click()
+
+    // Modal
+    await expect(page.locator('#bookmarkLabel >> nth=0')).toBeVisible()
+    await page.locator('#bookmarkLabel').first().fill('My Custom Link')
+    await page.locator('#bookmarkUrl').first().fill('google.com')
+    await page
+      .locator('.usa-form > .usa-button-group > li > .usa-button')
+      .first()
+      .click()
+
+    // Check that custom link is in the collection
+    await expect(
+      page.locator('text=My Custom Link(opens in a new window)')
+    ).toBeVisible()
+
+    // Edit custom link
+    await page.locator('[aria-label="Edit this link"]').first().click()
+
+    await page
+      .locator(
+        'div.is-visible > div.usa-modal-overlay > div > div.usa-modal__content > div.usa-modal__main > form > input'
+      )
+      .first()
+      .fill('Edited custom link name')
+
+    await page
+      .locator(
+        'div.is-visible > div.usa-modal-overlay > div > div.usa-modal__content > div.usa-modal__main > form > ul.usa-button-group > li > button:has-text("Save custom link")'
+      )
+      .click()
+
+    await expect(
+      page.locator('text=Edited custom link name(opens in a new window)')
+    ).toBeVisible()
+
+    // Delete custom link
+    await page.locator('[aria-label="Edit this link"]').first().click()
+
+    await page
+      .locator(
+        'div.is-visible > div.usa-modal-overlay > div > div.usa-modal__content > div.usa-modal__main > form > ul.usa-button-group > li > button:has-text("Delete")'
+      )
+      .click()
+
+    await expect(
+      page.locator('text=Edited custom link name(opens in a new window)')
+    ).toBeHidden()
+  })
+
+  test('can edit an existing collection title', async ({ page, loginPage }) => {
+    // Login and check that user is in My Space
+    await loginPage.login('user1', 'user1pass')
+    await expect(page.locator('text=WELCOME, BERNIE')).toBeVisible()
+    await expect(page.locator('text=Example Collection')).toBeVisible()
+
+    // Update Example Collection title
+    await page.locator('[aria-label="Collection Settings"]').first().click()
+    await page.locator('text=Edit collection title').click()
+    await page
+      .locator('[placeholder="Name this collection"]')
+      .fill('Updated Collection Title')
+    await page.locator('text=Save name').click()
+
+    await expect(page.locator('text=Updated Collection Title')).toBeVisible()
+  })
+
+  test('can remove multiple links at once from an existing collection', async ({
+    page,
+    loginPage,
+  }) => {
+    // Login and check that user is in My Space
+    await loginPage.login('user1', 'user1pass')
+    await expect(page.locator('text=WELCOME, BERNIE')).toBeVisible()
+    await expect(page.locator('text=Updated Collection Title')).toBeVisible()
+
+    // This line is repeated because after the first click, the first 'x' disappears
+    // and the second bookmark in the collection becomes the first
+    await page.locator('[aria-label="Remove this link"]').first().click()
+    await page.locator('[aria-label="Remove this link"]').first().click()
+
+    await expect(
+      page.locator('text=Webmail(opens in a new window)')
+    ).toBeHidden()
+    await expect(page.locator('text=MyPay(opens in a new window)')).toBeHidden()
+
+    // 'Undo remove' option disappears after 3 seconds, so we wait for 4 seconds
+    // before performing check
+    await waitFor(4000)
+    await expect(page.locator('text=Undo remove')).toBeHidden()
+  })
+
+  test('can delete an existing collection', async ({ page, loginPage }) => {
+    // Login and check that user is in My Space
+    await loginPage.login('user1', 'user1pass')
+    await expect(page.locator('text=WELCOME, BERNIE')).toBeVisible()
+    await expect(page.locator('text=Updated Collection Title')).toBeVisible()
+
+    // Cancel
+    await page.locator('[aria-label="Collection Settings"]').first().click()
+    await page.locator('button:has-text("Delete this collection")').click()
+    await page
+      .locator(
+        'div.is-visible > div.usa-modal-overlay > div > div.usa-modal__content > div.usa-modal__main > div.usa-modal__footer > ul > li > button:has-text("Cancel")'
+      )
+      .click()
+    await expect(page.locator('text=Updated Collection Title')).toBeVisible()
+
+    // Delete
+    await page.locator('[aria-label="Collection Settings"]').first().click()
+    await page.locator('button:has-text("Delete this collection")').click()
+    await page
+      .locator(
+        'div.is-visible > div.usa-modal-overlay > div > div.usa-modal__content > div.usa-modal__main > div.usa-modal__footer > ul > li > button:has-text("Delete")'
+      )
+      .click()
+    await expect(page.locator('text=Updated Collection Title')).toBeHidden()
   })
 })
---
+++ e2e/playwright/portal-client/tests/sites-and-apps.spec.ts
@@ -0,0 +1,164 @@
+import { test as base } from '@playwright/test'
+import {
+  fixtures,
+  TestingLibraryFixtures,
+} from '@playwright-testing-library/test/fixture'
+
+import { LoginPage } from '../../models/Login'
+import { seedDB } from '../database/seedMongo'
+
+type CustomFixtures = {
+  loginPage: LoginPage
+}
+
+const test = base.extend<TestingLibraryFixtures & CustomFixtures>({
+  ...fixtures,
+  loginPage: async ({ page, context }, use) => {
+    await use(new LoginPage(page, context))
+  },
+})
+
+const { describe, expect } = test
+
+test.beforeAll(async () => {
+  await seedDB()
+})
+
+describe('Sites & Applications', () => {
+  test('can visit the Sites & Applications page', async ({
+    page,
+    loginPage,
+  }) => {
+    // Login and check that user is in My Space
+    await loginPage.login('user1', 'user1pass')
+    await expect(page.locator('text=WELCOME, BERNIE')).toBeVisible()
+
+    // Navigate to /sites-and-applications using the side nav
+    await page.locator('text=All sites & applications').click()
+    await expect(
+      page.locator('h2:has-text("Sites & Applications")')
+    ).toBeVisible()
+
+    // Check that default collections are displaying
+    await expect(page.locator('text=Personnel & Administration')).toBeVisible()
+    await expect(page.locator('text=Finance & Travel')).toBeVisible()
+    await expect(page.locator('text=Public Military Websites')).toBeVisible()
+
+    // Toggle sorting
+    await page.locator('text=Sort alphabetically').click()
+    await expect(page.locator('text=Sort alphabetically')).toBeDisabled()
+    await expect(
+      page.locator(
+        'text=Provides direct Civil Engineer information management support to active Air Force units, the Air National Guard, and the Air Force Reserve, during peace and war, at fixed main bases, bare bases, and deployed locations.'
+      )
+    ).toBeVisible()
+
+    await page.locator('text=Sort by type').click()
+    await expect(page.locator('text=Sort by type')).toBeDisabled()
+    await expect(
+      page.locator(
+        'text=Provides direct Civil Engineer information management support to active Air Force units, the Air National Guard, and the Air Force Reserve, during peace and war, at fixed main bases, bare bases, and deployed locations.'
+      )
+    ).toBeHidden()
+  })
+
+  test('can add collections from the Sites & Applications page to My Space', async ({
+    page,
+    loginPage,
+  }) => {
+    // Login and check that user is in My Space
+    await loginPage.login('user1', 'user1pass')
+    await expect(page.locator('text=WELCOME, BERNIE')).toBeVisible()
+    await expect(page.locator('text=Example Collection')).toBeVisible()
+
+    // Click 'Add Section' and navigate to /sites-and-applications
+    await page.locator('text=Add section').click()
+    await page.locator('text=Select collection from template').click()
+    await expect(page).toHaveURL(
+      'http://localhost:3000/sites-and-applications?selectMode=true'
+    )
+
+    // Add three collections from Sites & Apps, unselect one, and navigate back to My Space
+    await page
+      .locator('[aria-label="Select collection Personnel \\& Administration"]')
+      .click()
+    await page.locator('[aria-label="Select collection Career"]').click()
+    await page
+      .locator('[aria-label="Select collection Medical \\& Dental"]')
+      .click()
+    await expect(page.locator('text=3 collections selected')).toBeVisible()
+    await page
+      .locator('[aria-label="Unselect collection Medical \\& Dental"]')
+      .click()
+    await expect(page.locator('text=2 collections selected')).toBeVisible()
+    await page.locator('text=Add selected').click()
+    await expect(page).toHaveURL('http://localhost:3000/')
+
+    // Check that new collections are in My Space
+    await expect(page.locator('text=Personnel & Administration')).toBeVisible()
+    await expect(page.locator('text=Career')).toBeVisible()
+  })
+
+  test('can add links to a new collection from the Sites & Applications page', async ({
+    page,
+    loginPage,
+  }) => {
+    // Login and check that user is in My Space
+    await loginPage.login('user1', 'user1pass')
+    await expect(page.locator('text=WELCOME, BERNIE')).toBeVisible()
+    await expect(page.locator('text=Example Collection')).toBeVisible()
+
+    // Navigate to /sites-and-applications
+    await page.locator('text=All sites & applications').click()
+    await expect(page).toHaveURL('http://localhost:3000/sites-and-applications')
+
+    // Add the ACES bookmark to a new collection in My Space
+    await page.locator('text=Sort alphabetically').click()
+    await page
+      .locator(
+        'text=ACES(opens in a new window)Provides direct Civil Engineer information management >> [data-testid="button"]'
+      )
+      .click()
+    await page.locator('text=Add to new collection').click()
+    await expect(page).toHaveURL('http://localhost:3000/')
+    await page
+      .locator('[placeholder="Name this collection"]')
+      .fill('Test adding from sites and apps')
+    await page.locator('text=Save name').click()
+    await expect(
+      page.locator('text=Test adding from sites and apps')
+    ).toBeVisible()
+    await expect(page.locator('text=ACES(opens in a new window)')).toBeVisible()
+  })
+
+  test('can add links to an existing collection from the Sites & Applications page', async ({
+    page,
+    loginPage,
+  }) => {
+    // Login and check that user is in My Space
+    await loginPage.login('user1', 'user1pass')
+    await expect(page.locator('text=WELCOME, BERNIE')).toBeVisible()
+    await expect(page.locator('text=Example Collection')).toBeVisible()
+
+    // Navigate to /sites-and-applications
+    await page.locator('text=All sites & applications').click()
+    await expect(page).toHaveURL('http://localhost:3000/sites-and-applications')
+
+    // Add 'Air University' bookmark to 'Example Collection' in My space
+    await page.locator('text=Sort alphabetically').click()
+    await page
+      .locator(
+        'text=Air University(opens in a new window)A center for professional military educatio >> [data-testid="button"]'
+      )
+      .click()
+    await page.locator('text=Example Collection').click()
+
+    // Navigate back to My Space
+    await page.locator('[data-testid="sidenav"] >> text=My Space').click()
+    await expect(page).toHaveURL('http://localhost:3000/')
+    await expect(page.locator('text=Example Collection')).toBeVisible()
+    await expect(
+      page.locator('text=Air University(opens in a new window)')
+    ).toBeVisible()
+  })
+})
